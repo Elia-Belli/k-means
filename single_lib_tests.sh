1debcff5b2e2247c4baac7bab3dc1ee1265a3dca
CiottoloMaggico <fantozz2003@outlook.com>
2025-02-07 18:36:54 +0100

    config for cluster

--- single_lib_tests.sh
+++ single_lib_tests.sh
@@ -7,57 +7,70 @@ for ((i=0; i < TEST_RUN; i++));
   do
 
     if [ $RUN_SEQUENTIAL_TESTS == true ]; then
-      echo "[${i}] Running Sequential version"
+      VERSION="seq"
+      echo "[${i}] Running ${VERSION} version"
+
       for ((j=0; j < INPUT_NUM; j++));
       do
-        echo "[SEQUENTIAL] Running test ${j}"
-          {\
-            ./bin/KMEANS_seq "${INPUT[j]}" "${K[j]}" "$ITER" "$MIN_CHANGES" "$MAX_DIST" "${OUT_DIR}KMEANS_seq_${j}.txt"; \
-            printf "\n"; \
-          } >> "${TEST_RESULTS}input_${j}.txt"
+        echo "[${VERSION}] Running test ${j}"
+        {\
+          printf "${VERSION},"; \
+          ./bin/KMEANS_seq ${INPUT[j]} ${K[j]} ${ITER} ${MIN_CHANGES} ${MAX_DIST} ${OUT_DIR}KMEANS_${VERSION}_${j}.txt; \
+          printf "\n"; \
+        } >> "${TEST_RESULTS}input_${j}.csv"
       done
-      echo "[${i}] Sequential runs completed"
+      echo "[${i}] ${VERSION} runs completed"
     fi
 
     if [ $RUN_MPI_TESTS == true ]; then
-      echo "[${i}] Running MPI version"
+      VERSION="mpi"
+      echo "[${i}] Running ${VERSION} version"
+
       for ((j=0; j < INPUT_NUM; j++));
       do
-        echo "[MPI] Running test ${j}"
+        echo "[${VERSION}] Running test ${j}"
         {\
-          mpirun --np "${MPI_PROCESSES}" --oversubscribe ./bin/KMEANS_mpi "${INPUT[j]}" "${K[j]}" "$ITER" "$MIN_CHANGES" "$MAX_DIST" "${OUT_DIR}KMEANS_mpi_${j}.txt" 2>"${OUT_DIR}KMEANS_mpi_${j}_error.txt"; \
+          printf "${VERSION},"; \
+          mpirun --bind-to none --np "${MPI_PROCESSES}" --oversubscribe \
+          ./bin/KMEANS_mpi ${INPUT[j]} ${K[j]} ${ITER} ${MIN_CHANGES} ${MAX_DIST} ${OUT_DIR}KMEANS_${VERSION}_${j}.txt; \
           printf ","; \
-          ./bin/compare "${OUT_DIR}KMEANS_seq_${j}.txt" "${OUT_DIR}KMEANS_mpi_${j}.txt" >> "${TEST_RESULTS}input_${j}.txt"
-        } >> "${TEST_RESULTS}input_${j}.txt"
+          ./bin/compare "${OUT_DIR}KMEANS_seq_${j}.txt" "${OUT_DIR}KMEANS_${VERSION}_${j}.txt"; \
+        } >> "${TEST_RESULTS}input_${j}.csv"
       done
-      echo "[${i}] Mpi runs completed"
+      echo "[${i}] ${VERSION} runs completed"
     fi
 
     if [ $RUN_OMP_TESTS == true ]; then
-      echo "[${i}] Running OpenMP version"
+      VERSION="omp"
+      echo "[${i}] Running ${VERSION} version"
+
       for ((j=0; j < INPUT_NUM; j++));
       do
-        echo "[OPENMP] Running test ${j}"
+        echo "[${VERSION}] Running test ${j}"
         {\
-          ./bin/KMEANS_omp "${INPUT[j]}" "${K[j]}" "$ITER" "$MIN_CHANGES" "$MAX_DIST" "${OUT_DIR}KMEANS_omp_${j}.txt"; \
+          printf "${VERSION},"; \
+          ./bin/KMEANS_omp ${INPUT[j]} ${K[j]} ${ITER} ${MIN_CHANGES} ${MAX_DIST} ${OUT_DIR}KMEANS_${VERSION}_${j}.txt; \
           printf ","; \
-          ./bin/compare "${OUT_DIR}KMEANS_seq_${j}.txt" "${OUT_DIR}KMEANS_omp_${j}.txt" >> "${TEST_RESULTS}input_${j}.txt"
-        } >> "${TEST_RESULTS}input_${j}.txt"
+          ./bin/compare "${OUT_DIR}KMEANS_seq_${j}.txt" "${OUT_DIR}KMEANS_${VERSION}_${j}.txt"; \
+        } >> "${TEST_RESULTS}input_${j}.csv"
       done
-      echo "[${i}] OpenMP runs completed"
+      echo "[${i}] ${VERSION} runs completed"
     fi
 
     if [ $RUN_CUDA_TESTS == true ]; then
-      echo "[${i}] Running Cuda version"
+      VERSION="cuda"
+      echo "[${i}] Running ${VERSION} version"
+
       for ((j=0; j < INPUT_NUM; j++));
       do
-        echo "[CUDA] Running test ${j}"
+        echo "[${VERSION}] Running test ${j}"
         {\
-          ./bin/KMEANS_cuda "${INPUT[j]}" "${K[j]}" "$ITER" "$MIN_CHANGES" "$MAX_DIST" "${OUT_DIR}KMEANS_cuda_${j}.txt"; \
+          printf "${VERSION},"; \
+          ./bin/KMEANS_cuda ${INPUT[j]} ${K[j]} ${ITER} ${MIN_CHANGES} ${MAX_DIST} ${OUT_DIR}KMEANS_${VERSION}_${j}.txt; \
           printf ","; \
-          ./bin/compare "${OUT_DIR}KMEANS_seq_${j}.txt" "${OUT_DIR}KMEANS_cuda_${j}.txt" >> "${TEST_RESULTS}input_${j}.txt"
-        } >> "${TEST_RESULTS}input_${j}.txt"
+          ./bin/compare "${OUT_DIR}KMEANS_seq_${j}.txt" "${OUT_DIR}KMEANS_${VERSION}_${j}.txt"; \
+        } >> "${TEST_RESULTS}input_${j}.csv"
       done
-      echo "[${i}] Cuda runs completed"
+      echo "[${i}] ${VERSION} runs completed"
     fi
 done
