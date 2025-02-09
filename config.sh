3d60889b1146bec2f8e07ef764eea7072cdb453b
Elia-Belli <elia.belli.2158@gmail.com>
2025-02-09 18:51:16 +0100

    add cuda tests

--- config.sh
+++ config.sh
@@ -10,10 +10,10 @@ OMP_NUM_THREADS=32
 MPI_PROCESSES_COMBINED=8 # only in local
 OMP_NUM_THREADS_COMBINED=32 # only in local
 
-RUN_SEQUENTIAL_TESTS=true
-RUN_MPI_TESTS=true
-RUN_OMP_TESTS=true
-RUN_CUDA_TESTS=false
+RUN_SEQUENTIAL_TESTS=false
+RUN_MPI_TESTS=false
+RUN_OMP_TESTS=false
+RUN_CUDA_TESTS=true
 RUN_MPI_OMP_TESTS=false
 RUN_MPI_PARALLEL_TESTS=false
 
@@ -30,4 +30,6 @@ STRONG_SCALING_INPUT=5
 
 INPUT_WEAK=("${TEST_DIR}input3125x100.inp" "${TEST_DIR}input6250x100.inp" "${TEST_DIR}input12500x100.inp" "${TEST_DIR}input25000x100.inp" "${TEST_DIR}input37500x100.inp" "${TEST_DIR}input50000x100.inp" "${TEST_DIR}input62500x100.inp" "${TEST_DIR}input75000x100.inp" "${TEST_DIR}input87500x100.inp")
 WEAK_SCALING_THREADS=(1 2 4 8 12 16 20 24 28)
+
+INPUT_CUDA=("${TEST_DIR}input3125x100.inp" "${TEST_DIR}input6250x100.inp" "${TEST_DIR}input12500x100.inp" "${TEST_DIR}input25000x100.inp" "${TEST_DIR}input37500x100.inp" "${TEST_DIR}input50000x100.inp" "${TEST_DIR}input62500x100.inp" "${TEST_DIR}input75000x100.inp" "${TEST_DIR}input87500x100.inp" "${TEST_DIR}input100D2.inp")
 # ---------------------------------------------------------
