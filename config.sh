5c8128722d9ce99a88cbce9d223d290e4178533d
Elia-Belli <elia.belli.2158@gmail.com>
2025-02-09 18:56:17 +0100

    update cuda tests

--- config.sh
+++ config.sh
@@ -31,5 +31,5 @@ STRONG_SCALING_INPUT=5
 INPUT_WEAK=("${TEST_DIR}input3125x100.inp" "${TEST_DIR}input6250x100.inp" "${TEST_DIR}input12500x100.inp" "${TEST_DIR}input25000x100.inp" "${TEST_DIR}input37500x100.inp" "${TEST_DIR}input50000x100.inp" "${TEST_DIR}input62500x100.inp" "${TEST_DIR}input75000x100.inp" "${TEST_DIR}input87500x100.inp")
 WEAK_SCALING_THREADS=(1 2 4 8 12 16 20 24 28)
 
-INPUT_CUDA=("${TEST_DIR}input3125x100.inp" "${TEST_DIR}input6250x100.inp" "${TEST_DIR}input12500x100.inp" "${TEST_DIR}input25000x100.inp" "${TEST_DIR}input37500x100.inp" "${TEST_DIR}input50000x100.inp" "${TEST_DIR}input62500x100.inp" "${TEST_DIR}input75000x100.inp" "${TEST_DIR}input87500x100.inp" "${TEST_DIR}input100D2.inp")
+INPUT_CUDA=("${TEST_DIR}input3125x100.inp" "${TEST_DIR}input6250x100.inp" "${TEST_DIR}input12500x100.inp" "${TEST_DIR}input25000x100.inp" "${TEST_DIR}input37500x100.inp" "${TEST_DIR}input50000x100.inp" "${TEST_DIR}input62500x100.inp" "${TEST_DIR}input75000x100.inp" "${TEST_DIR}input87500x100.inp" "${TEST_DIR}input100D2.inp" "${TEST_DIR}input250000x100.inp" "${TEST_DIR}input500000x100.inp" "${TEST_DIR}input1000000x100.inp")
 # ---------------------------------------------------------
