d5bf41c721c5d06a8b13db50530b01daa9f689d0
Elia-Belli <elia.belli.2158@gmail.com>
2025-02-10 11:00:15 +0100

    update mpi+omp weak test

--- run_combined_weak.sh
+++ run_combined_weak.sh
@@ -5,7 +5,7 @@ for ((i=0; i < 10; i++));
 do
   condor_submit job.parallel \
   -append "executable = weak_scaling_combined.sh" \
-  -append "arguments = ${STRONG_SCALING_THREADS[i]}" \
-  -append "machine_count = ${STRONG_SCALING_THREADS[i]}" \
+  -append "arguments = ${WEAK_SCALING_THREADS[i]}" \
+  -append "machine_count = ${WEAK_SCALING_THREADS[i]}" \
   -append 'request_cpus = 32'
 done
--- weak_scaling_combined.sh
+++ weak_scaling_combined.sh
@@ -11,29 +11,18 @@ NUM_MACHINES=$1
 
 for ((i=0; i < TEST_RUN; i++));
   do
-    ITERATIONS=${#INPUT_WEAK[@]}
 
     if [ "$RUN_MPI_OMP_TESTS" == true ]; then
         VERSION="mpi+omp"
-        echo "[${i}] Running ${VERSION} version"
+        echo "[${i}] Running ${VERSION} version on ${INPUT_WEAK[${WEAK_SCALING_THREADS[$NUM_MACHINES]}]}"
 
-        for ((j=0; j < ITERATIONS; j++));
-            do
-                echo "[${VERSION}] Running test on ${INPUT_WEAK[j]}"
+        OUTPUT=$(./bin/KMEANS_mpi+omp "${INPUT_WEAK[${WEAK_SCALING_THREADS[$NUM_MACHINES]}]}"\
+         100 "$ITER" "$MIN_CHANGES" "$MAX_DIST" "${OUT_DIR}KMEANS_${VERSION}_${j}.txt")
 
-                OUTPUT=$(\
-                ./bin/KMEANS_mpi+omp "${INPUT_WEAK[j]}" 100 "$ITER" "$MIN_CHANGES" "$MAX_DIST" "${OUT_DIR}KMEANS_${VERSION}_${j}.txt"\
-                )
-
-                if [ $_CONDOR_PROCNO == 0 ]; then
-                    printf "%s " "${OUTPUT}" >> "${TEST_RESULTS}input_weak_${VERSION}_${NUM_MACHINES}.csv"
-                    cp "${TEST_RESULTS}input_weak_${VERSION}_${NUM_MACHINES}.csv" "${HOME}/k-means/tests"
-
-                    if [ $j == $((ITERATIONS -1)) ]; then
-                        printf "\n" "${OUTPUT}" >> "${TEST_RESULTS}input_weak_${VERSION}_${NUM_MACHINES}.csv"
-                    fi
-                fi
-            done
+        if [ $_CONDOR_PROCNO == 0 ]; then
+            printf "%s\n" "${OUTPUT}" >> "${TEST_RESULTS}input_weak_${VERSION}_${NUM_MACHINES}.csv"
+            cp "${TEST_RESULTS}input_weak_${VERSION}_${NUM_MACHINES}.csv" "${HOME}/k-means/tests"
+        fi
 
         printf "%s\n" "${OUTPUT}" >> "${TEST_RESULTS}${VERSION}.csv"
 
